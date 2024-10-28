45ee03fa8eea5f6b4ee2fb78f60f2a39aff32622
이상우 <swoody1101@gmail.com>
2024-10-28 19:39:49 +0900

    알림창을 띄운 후 페이지 이동을 제어하기 위한 alertLocation 및 alertBack static 함수 작성

---
+++ JSP/MustHaveJSP/src/main/java/utils/JSFunction.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.io.IOException;
+
+import jakarta.servlet.jsp.JspWriter;
+
+public class JSFunction {
+	// 메시지 알림창을 띄운 후 명시한 URL로 이동합니다.
+	public static void alertLocation(String msg, String url, JspWriter out) {
+		try {
+			String script = ""
+				+ "<script>"
+				+ "    alert('" + msg + "');"
+				+ "    location.href='" + url + "';"
+				+ "</script>";
+			out.println(script);
+		} catch (IOException e) {}
+	}
+	
+	// 메시지 알림창을 띄운 후 이전 페이지로 돌아갑니다.
+	public static void alertBack(String msg, JspWriter out) {
+		try {
+			String script = ""
+					+ "<script>"
+					+ "    alert('" + msg + "');"
+					+ "</script>";
+			out.println(script);
+		} catch (Exception e) {}
+	}
+}
