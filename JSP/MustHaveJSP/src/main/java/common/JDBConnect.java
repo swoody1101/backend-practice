56d6f77657daa1f1869068b3d3b5bca9b7c0c498
이상우 <swoody1101@gmail.com>
2024-11-01 20:45:53 +0900

    Oracle 연결 관리 클래스 작성

---
+++ JSP/MustHaveJSP/src/main/java/common/JDBConnect.java
@@ -0,0 +1,44 @@
+package common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JDBConnect {
+	public Connection con;
+	public Statement stmt;
+	public PreparedStatement pstmt;
+	public ResultSet rs;
+
+	public JDBConnect() {
+		try {
+			// JDBC 드라이버 로드
+			Class.forName("oracle.jdbc.OracleDriver");
+
+			// DB에 연결
+			String url = "jdbc:oracle:thi:@localhost:1521:xe";
+			String id = "swoody";
+			String pwd = "0000";
+			con = DriverManager.getConnection(url, id, pwd);
+
+			System.out.println("DB 연결 성공(기본 생성자)");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void close() {
+		try {
+			if (rs != null) rs.close();
+			if (stmt != null) stmt.close();
+			if (pstmt != null) pstmt.close();
+			if (con != null) con.close();
+			
+			System.out.println("JDBC 자원 해제");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
