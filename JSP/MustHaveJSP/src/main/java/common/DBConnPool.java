31e3a606f0dc0acefd52d00167a6e739ef5dfab1
이상우 <swoody1101@gmail.com>
2024-11-01 23:36:08 +0900

    커넥션 풀 사용 DB 연결 클래스 작성

---
+++ JSP/MustHaveJSP/src/main/java/common/DBConnPool.java
@@ -0,0 +1,47 @@
+package common;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBConnPool {
+	public Connection con;
+	public Statement stmt;
+	public PreparedStatement pstmt;
+	public ResultSet rs;
+
+	public DBConnPool() {
+		try {
+			// 커넥션 풀(DataSource 얻기)
+			Context initCtx = new InitialContext();
+			Context ctx = (Context) initCtx.lookup("java:comp/env");
+			DataSource source = (DataSource) ctx.lookup("dbcp_myoracle");
+
+			// 커넥션 풀을 통해 연결 얻기
+			con = source.getConnection();
+
+			System.out.println("DB 커넥션 풀 연결 실패");
+		} catch (Exception e) {
+			System.out.println("DB 커넥션 풀 연결 실패");
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+		try {
+			if (rs != null) rs.close();
+			if (stmt != null) stmt.close();
+			if (pstmt != null) pstmt.close();
+			if (con != null) con.close();
+			
+			System.out.println("DB 커넥션 풀 자원 반납");
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+	}
+}
