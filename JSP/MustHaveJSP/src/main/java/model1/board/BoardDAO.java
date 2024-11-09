3d9fb2521075a372c0c8fd03cd1ab688efbd8046
이상우 <swoody1101@gmail.com>
2024-11-09 17:47:41 +0900

    게시물 목록을 가져오기 위한 selectList() 함수 추가

--- JSP/MustHaveJSP/src/main/java/model1/board/BoardDAO.java
+++ JSP/MustHaveJSP/src/main/java/model1/board/BoardDAO.java
@@ -1,6 +1,8 @@
 package model1.board;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import common.JDBConnect;
 import jakarta.servlet.ServletContext;
@@ -29,5 +31,34 @@ public class BoardDAO extends JDBConnect {
 		}
 
 		return totalCount;
+	}
+
+	public List<BoardDTO> selectList(Map<String, Object> map) {
+		List<BoardDTO> bbs = new Vector<BoardDTO>();
+
+		String query = "SELECT * FROM board ";
+		if (map.get("searchWord") != null) {
+			query += "WHERE " + map.get("searchField") + " LIKE '%" + map.get("searchWord") + "%'";
+		}
+
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				BoardDTO dto = new BoardDTO();
+				dto.setNum(rs.getString("num"));
+				dto.setTitle(rs.getString("title"));
+				dto.setContent(rs.getString("content"));
+				dto.setPostdate(rs.getDate("postdate"));
+				dto.setId(rs.getString("id"));
+				dto.setVisitcount(rs.getString("visitcount"));
+				bbs.add(dto);
+			}
+		} catch (Exception e) {
+			System.out.println("게시물 리스트를");
+			e.printStackTrace();
+		}
+
+		return bbs;
 	}
 }
