366d4f945973bb24e6bd6582f8ac02b6e474dc36
이상우 <swoody1101@gmail.com>
2024-11-09 17:45:57 +0900

    게시물 개수를 세기 위한 selectCount() 함수 추가

--- JSP/MustHaveJSP/src/main/java/model1/board/BoardDAO.java
+++ JSP/MustHaveJSP/src/main/java/model1/board/BoardDAO.java
@@ -1,10 +1,33 @@
 package model1.board;
 
+import java.util.Map;
+
 import common.JDBConnect;
 import jakarta.servlet.ServletContext;
 
 public class BoardDAO extends JDBConnect {
 	public BoardDAO(ServletContext application) {
 		super(application);
+	}
+
+	public int selectCount(Map<String, Object> map) {
+		int totalCount = 0;
+
+		String query = "SELECT COUNT(*) FROM board";
+		if (map.get("searchWord") != null) {
+			query += "WHERE " + map.get("searchFiled") + " " + "LIKE '%" + map.get("searchWord") + "%'";
+		}
+
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+			rs.next();
+			totalCount = rs.getInt(1);
+		} catch (Exception e) {
+			System.out.println("게시물 수를 구하는 중 예외 발생");
+			e.printStackTrace();
+		}
+
+		return totalCount;
 	}
 }
