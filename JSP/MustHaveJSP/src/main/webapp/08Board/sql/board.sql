3b0b3aef631fd3b122e2be858638952721ff53e2
이상우 <swoody1101@gmail.com>
2024-11-09 19:24:57 +0900

    board 테이블에 추가할 더미 데이터

---
+++ JSP/MustHaveJSP/src/main/webapp/08Board/sql/board.sql
@@ -0,0 +1,10 @@
+INSERT INTO BOARD
+	VALUES (seq_board_num.nextval, '지금은 봄입니다', '봄의왈츠', 'swoody', sysdate, 0);
+INSERT INTO BOARD
+	VALUES (seq_board_num.nextval, '지금은 여름입니다', '여름향기', 'swoody', sysdate, 0);
+INSERT INTO BOARD
+	VALUES (seq_board_num.nextval, '지금은 가을입니다', '가을동화', 'swoody', sysdate, 0);
+INSERT INTO BOARD
+	VALUES (seq_board_num.nextval, '지금은 겨울입니다', '겨울연가', 'swoody', sysdate, 0);
+
+COMMIT;
