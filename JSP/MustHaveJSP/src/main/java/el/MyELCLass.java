c74ea008dbcf9462d342010112d37462428c3ee6
이상우 <swoody1101@gmail.com>
2024-11-20 15:30:06 +0900

    주민번호를 입력받아 성별을 반환하는 getGender() 함수 작성

--- JSP/MustHaveJSP/src/main/java/el/MyELCLass.java
+++ JSP/MustHaveJSP/src/main/java/el/MyELCLass.java
@@ -1,5 +1,20 @@
 package el;
 
 public class MyELCLass {
-	
+	public String getGender(String jumin) {
+		String returnStr = "";
+		int beginIdx = jumin.indexOf("-") + 1;
+		String genderStr = jumin.substring(beginIdx, beginIdx + 1);
+		int genderInt = Integer.parseInt(genderStr);
+
+		if (genderInt == 1 || genderInt == 3) {
+			returnStr = "남자";
+		} else if (genderInt == 2 || genderInt == 4) {
+			returnStr = "여자";
+		} else {
+			returnStr = "주민번호 오류입니다.";
+		}
+
+		return returnStr;
+	}
 }
