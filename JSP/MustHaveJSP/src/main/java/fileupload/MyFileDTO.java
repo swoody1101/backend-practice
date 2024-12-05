78906db77ba5ea835a5341b64a18ae7c8a61ba4f
이상우 <swoody1101@gmail.com>
2024-12-05 22:47:40 +0900

    myfile 테이블용 DTO 클래스 생성

---
+++ JSP/MustHaveJSP/src/main/java/fileupload/MyFileDTO.java
@@ -0,0 +1,59 @@
+package fileupload;
+
+public class MyFileDTO {
+	private String idx;
+	private String title;
+	private String cate;
+	private String ofile;
+	private String sfile;
+	private String postdate;
+
+	public String getIdx() {
+		return idx;
+	}
+
+	public void setIdx(String idx) {
+		this.idx = idx;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCate() {
+		return cate;
+	}
+
+	public void setCate(String cate) {
+		this.cate = cate;
+	}
+
+	public String getOfile() {
+		return ofile;
+	}
+
+	public void setOfile(String ofile) {
+		this.ofile = ofile;
+	}
+
+	public String getSfile() {
+		return sfile;
+	}
+
+	public void setSfile(String sfile) {
+		this.sfile = sfile;
+	}
+
+	public String getPostdate() {
+		return postdate;
+	}
+
+	public void setPostdate(String postdate) {
+		this.postdate = postdate;
+	}
+
+}
