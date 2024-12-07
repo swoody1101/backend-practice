2a27e7c7975c7017eaf4f91f1e621fce0f196a38
이상우 <swoody1101@gmail.com>
2024-12-07 22:54:33 +0900

    uploadFile 클래스 생성

--- JSP/MustHaveJSP/src/main/java/fileupload/FileUtil.java
+++ JSP/MustHaveJSP/src/main/java/fileupload/FileUtil.java
@@ -1,5 +1,23 @@
 package fileupload;
 
-public class FileUtil {
+import java.io.File;
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.Part;
+
+public class FileUtil {
+	public static String uploadFile(HttpServletRequest req, String sDirectory) throws ServletException, IOException {
+		Part part = req.getPart("ofile");
+		String partHeader = part.getHeader("content-disposition");
+		String[] phArr = partHeader.split("filename=");
+		String originalFileName = phArr[1].trim().replace("\"", "");
+		if (!originalFileName.isEmpty()) {
+			part.write(sDirectory + File.separator + originalFileName);
+		}
+		
+		return originalFileName;
+	}
 }
+
