640970e8f0c42805c9f8b957d19e3c22338ccb7b
이상우 <swoody1101@gmail.com>
2024-12-08 20:35:39 +0900

    파일명 변경을 위한 renameFile 메서드 생성

--- JSP/MustHaveJSP/src/main/java/fileupload/FileUtil.java
+++ JSP/MustHaveJSP/src/main/java/fileupload/FileUtil.java
@@ -2,6 +2,8 @@ package fileupload;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,8 +18,18 @@ public class FileUtil {
 		if (!originalFileName.isEmpty()) {
 			part.write(sDirectory + File.separator + originalFileName);
 		}
-		
+
 		return originalFileName;
 	}
-}
 
+	public static String renameFile(String sDirectory, String fileName) {
+		String ext = fileName.substring(fileName.lastIndexOf("."));
+		String now = new SimpleDateFormat("yyyyMMdd_HmsS").format(new Date());
+		String newFileName = now + ext;
+		File oldFile = new File(sDirectory + File.separator + fileName);
+		File newFile = new File(sDirectory + File.separator + newFileName);
+		oldFile.renameTo(newFile);
+
+		return newFileName;
+	}
+}
