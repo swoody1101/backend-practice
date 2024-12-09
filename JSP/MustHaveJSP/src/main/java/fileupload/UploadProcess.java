f5770afba89db4c4aef43d6f19ddfd7d92623bdc
이상우 <swoody1101@gmail.com>
2024-12-09 22:30:09 +0900

    요청명 매핑 및 멀티파트 요청 설정

--- JSP/MustHaveJSP/src/main/java/fileupload/UploadProcess.java
+++ JSP/MustHaveJSP/src/main/java/fileupload/UploadProcess.java
@@ -1,7 +1,11 @@
 package fileupload;
 
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
+@WebServlet("/13FileUpload/UploadProcess.do")
+@MultipartConfig(maxFileSize = 1024 * 1024 * 1, maxRequestSize = 1024 * 1024 * 10)
 public class UploadProcess extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
