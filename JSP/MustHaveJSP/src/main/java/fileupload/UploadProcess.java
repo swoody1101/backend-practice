6ea5e734799afaec36b08a2253d49b16ae85a077
이상우 <swoody1101@gmail.com>
2024-12-10 15:19:35 +0900

    파일 DB 저장을 위한 insertMyFile 메서드 생성

--- JSP/MustHaveJSP/src/main/java/fileupload/UploadProcess.java
+++ JSP/MustHaveJSP/src/main/java/fileupload/UploadProcess.java
@@ -1,12 +1,39 @@
 package fileupload;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/13FileUpload/UploadProcess.do")
 @MultipartConfig(maxFileSize = 1024 * 1024 * 1, maxRequestSize = 1024 * 1024 * 10)
 public class UploadProcess extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	private void insertMyFile(HttpServletRequest req, String oFileName, String sFileName) {
+		String title = req.getParameter("title");
+		String[] cateArray = req.getParameterValues("cate");
+		StringBuffer cateBuf = new StringBuffer();
+		if (cateArray == null) {
+			cateBuf.append("선택한 항목 없음");
+		} else {
+			for (String s : cateArray) {
+				cateBuf.append(s + ", ");
+			}
+		}
+
+		MyFileDTO dto = new MyFileDTO();
+		dto.setTitle(title);
+		dto.setCate(cateBuf.toString());
+		dto.setOfile(oFileName);
+		dto.setSfile(sFileName);
+		
+		MyFileDAO dao = new MyFileDAO();
+		dao.insertFile(dto);
+		dao.close();
+	}
 }
