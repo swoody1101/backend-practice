a60deaf41ff18449abf0a8824e6300852a76c8fd
swoody1101 <swoody1101@gmail.com>
2025-07-01 16:06:26 +0900

    숫자 및 문자열의 다앙한 함수 활용 실습 예제 작성

---
+++ Python/02/format04.py
@@ -0,0 +1,15 @@
+# 조합하기
+output_h = "{:+5d}".format(52)
+output_i = "{:+5d}".format(-52)
+output_j = "{:=+5d}".format(52)
+output_k = "{:=+5d}".format(-52)
+output_l = "{:+05d}".format(52)
+output_m = "{:+05d}".format(-52)
+
+print("# 조합하기")
+print(output_h)
+print(output_i)
+print(output_j)
+print(output_k)
+print(output_l)
+print(output_m)
---
+++ Python/02/format05.py
@@ -0,0 +1,9 @@
+output_a = "{:f}".format(52.273)
+output_b = "{:15f}".format(52.273)
+output_c = "{:+15f}".format(52.273)
+output_d = "{:+015f}".format(52.273)
+
+print(output_a)
+print(output_b)
+print(output_c)
+print(output_d)
