6cd9851444700b53d4aecccb0476a4c6360d6990
swoody1101 <swoody1101@gmail.com>
2025-07-01 17:51:40 +0900

    if-else와 elif 구문 활용 실습 예제 작성

---
+++ Python/03/pass_keyword.py
@@ -0,0 +1,12 @@
+# 입력을 받습니다.
+number = input("정수 입력> ")
+number = int(number)
+
+# 조건문 사용
+if number > 0:
+    # 양수일 때: 아직 미구현 상태입니다.
+    pass
+else:
+    # 음수일 때: 아직 미구현 상태입니다.
+    # 아직 구현하지 않은 부분이라는 오류를 강제로 발생 시킬 수 있습니다.
+    raise NotImplementedError
