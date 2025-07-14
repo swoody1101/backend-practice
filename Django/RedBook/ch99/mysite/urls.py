1a8aa10ef5513a1b30cd215f47d64c08b501ec95
swoody1101 <swoody1101@gmail.com>
2025-07-14 17:43:26 +0900

    불필요한 import 삭제

--- Django/RedBook/ch99/mysite/urls.py
+++ Django/RedBook/ch99/mysite/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from polls import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
