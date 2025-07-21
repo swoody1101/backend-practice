74f78be67f5d547d047a294809bdc1dbbbf4803b
swoody1101 <swoody1101@gmail.com>
2025-07-21 15:05:21 +0900

    Question 클래스 각 필드를 분리해서 출력

--- Django/RedBook/ch99/polls/admin.py
+++ Django/RedBook/ch99/polls/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
+
 # Register your models here.
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        ("Question Statement", {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"]}),
+    ]
+
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
