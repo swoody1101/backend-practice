8a64fd9064048be3deeb498d0af40a1f1aec6e35
swoody1101 <swoody1101@gmail.com>
2025-07-21 15:18:37 +0900

    Question 및 Choice를 한 화면에서 변경할 수 있도록 수정

--- Django/RedBook/ch99/polls/admin.py
+++ Django/RedBook/ch99/polls/admin.py
@@ -3,11 +3,17 @@ from polls.models import Question, Choice
 
 
 # Register your models here.
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Question Statement", {"fields": ["question_text"]}),
         ("Date Information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
