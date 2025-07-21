52352611f71cac948060e6bd2d866484c9bae6bd
swoody1101 <swoody1101@gmail.com>
2025-07-21 16:21:57 +0900

    polls/admin.py 변경 내역 정리

--- Django/RedBook/ch99/polls/admin.py
+++ Django/RedBook/ch99/polls/admin.py
@@ -1,22 +1,25 @@
 from django.contrib import admin
+
+# Registser your models here.
 from polls.models import Question, Choice
 
 
-# Register your models here.
+# class ChoiceInline(admin.StackedInline):
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 2
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    # fileds = ['pub_date, 'question_text']
     fieldsets = [
         ("Question Statement", {"fields": ["question_text"]}),
         ("Date Information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['questio_text']
+    list_display = ("question_text", "pub_date")
+    list_filter = ["pub_date"]
+    search_fields = ["questio_text"]
 
 
 admin.site.register(Question, QuestionAdmin)
