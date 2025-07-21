69fdbf3adab80b612fbf9c01bd8094a18d6f830f
swoody1101 <swoody1101@gmail.com>
2025-07-21 15:00:20 +0900

    Question 클래스 필드 순서 변경

--- Django/RedBook/ch99/polls/admin.py
+++ Django/RedBook/ch99/polls/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from polls.models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
